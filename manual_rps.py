85ee9d9153b2017113384ea49f3fa4134d47fcd3
chemi5t <umar.chemi5t@gmail.com>
2023-08-10 16:42:02 +0100

    Update file manual_rps.py to pass feedback checks.

--- manual_rps.py
+++ manual_rps.py
@@ -14,23 +14,31 @@ def get_user_choice(): # User to enter a choice from action_list where input is 
       else:
          print("Oops! That is not a valid input. Choose from 'rock, 'paper' or scissors.")
 
-def get_winner(computer_choice, user_choice): # Checks computers choice vs users and declares a winner
-    if computer_choice == "Rock" and user_choice == "Paper":
-        return "You won! User's Paper beats Rock."
-    elif computer_choice == "Rock" and user_choice == "Scissors":
-        return "You lost. Computer's Rock beats Scissors."
-    elif computer_choice == "Rock" and user_choice == "Rock":
-        return "It is a tie!"
-    elif computer_choice == "Paper" and user_choice == "Scissors":
-        return "You won! User's Scissors beat Paper."
-    elif computer_choice == "Paper" and user_choice == "Rock":
-        return "You lost. Computer's Paper beats Rock."
-    elif computer_choice == "Paper" and user_choice == "Paper":
-        return "It is a tie!"
-    elif computer_choice == "Scissors" and user_choice == "Rock":
-        return "You won! User's Rock beats Scissors."
-    elif computer_choice == "Scissors" and user_choice == "Paper":
-        return "You lost. Computer's Scissors beat Paper."
+# def get_winner(computer_choice, user_choice): # Checks computers choice vs users and declares a winner
+#     if computer_choice == "Rock" and user_choice == "Paper":
+#         return "You won! User's Paper beats Rock."
+#     elif computer_choice == "Rock" and user_choice == "Scissors":
+#         return "You lost. Computer's Rock beats Scissors."
+#     elif computer_choice == "Rock" and user_choice == "Rock":
+#         return "It is a tie!"
+#     elif computer_choice == "Paper" and user_choice == "Scissors":
+#         return "You won! User's Scissors beat Paper."
+#     elif computer_choice == "Paper" and user_choice == "Rock":
+#         return "You lost. Computer's Paper beats Rock."
+#     elif computer_choice == "Paper" and user_choice == "Paper":
+#         return "It is a tie!"
+#     elif computer_choice == "Scissors" and user_choice == "Rock":
+#         return "You won! User's Rock beats Scissors."
+#     elif computer_choice == "Scissors" and user_choice == "Paper":
+#         return "You lost. Computer's Scissors beat Paper."
+#     else:
+#         return "It is a tie!"
+
+def get_winner(computer_choice, user_choice): # Checks computers choice vs users and declare a winner
+    if computer_choice == "Rock" and user_choice == "Paper" or computer_choice == "Paper" and user_choice == "Scissors" or computer_choice == "Scissors" and user_choice == "Rock":
+        return "You won!"
+    elif computer_choice == "Rock" and user_choice == "Scissors" or computer_choice == "Paper" and user_choice == "Rock" or computer_choice == "Scissors" and user_choice == "Paper":
+        return "You lost."
     else:
         return "It is a tie!"
 
